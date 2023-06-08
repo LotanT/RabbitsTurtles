d91ba55c690b5b469d03943fe8357c88f26f42ee
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2023-06-08 12:40:02 +0300

    rebuild

--- app/src/services/socket.service.js
+++ app/src/services/socket.service.js
@@ -1,33 +1,34 @@
-import io from 'socket.io-client'
+import io from 'socket.io-client';
 
-export const socketService = createSocketService()
+export const socketService = createSocketService();
 
-const baseUrl = (process.env.NODE_ENV === 'production') ? '' : '//localhost:3001'
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://rabbitsvsturtlesapi.onrender.com'
+    : '//localhost:3001';
 
-
-socketService.setup()
-
+socketService.setup();
 
 function createSocketService() {
   var socket = null;
   const socketService = {
     setup() {
-      socket = io(baseUrl)
+      socket = io(baseUrl);
     },
     on(eventName, cb) {
-      socket.on(eventName, cb)
+      socket.on(eventName, cb);
     },
     off(eventName, cb = null) {
       if (!socket) return;
-      if (!cb) socket.removeAllListeners(eventName)
-      else socket.off(eventName, cb)
+      if (!cb) socket.removeAllListeners(eventName);
+      else socket.off(eventName, cb);
     },
     emit(eventName, data) {
-      socket.emit(eventName, data)
+      socket.emit(eventName, data);
     },
     terminate() {
-      socket = null
-    }
-  }
-  return socketService
+      socket = null;
+    },
+  };
+  return socketService;
 }
